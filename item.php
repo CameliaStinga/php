202306d41c517c1665ad9c89c1361b7fc711b673
Camelia STINGA <camelia.stinga@EDG>
2021-05-20 11:29:40 +0200

    item.php avec header et footer

---
+++ item.php
@@ -0,0 +1,9 @@
+<?php include ("header.php")?>
+<?php $Nom = "Alex";
+      $Prix=10;
+      $image='https://static3.depositphotos.com/1002188/144/i/600/depositphotos_1448005-stock-photo-smile.jpg';
+
+echo "$Nom tu dois payer $Prix euros! <img src = '$image' alt = smile/>";
+
+ ?>
+<?php include ("footer.php")?>
