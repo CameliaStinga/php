6d8830a4b88f3cbc55fc06abb31a70db15dd13d1
camelia.stinga <camelia.stinga@le-campus-numerique.fr>
2021-06-08 16:43:38 +0200

    requetes selection sur plusieurs tables et insertion et suppression

--- database.php
+++ database.php
@@ -12,27 +12,24 @@ function bddAccess() {
 <?php
 function allCustomers (){
     $bdd=bddAccess();
+
 // On récupère tout le contenu de la table customers (querry1);
+
 $r1 = $bdd->query('SELECT * FROM customers');
 
 // On affiche chaque entrée une à une
 while ($donnees = $r1->fetch())
 {
     ?>
-
         <strong>Client</strong> : <?php echo $donnees['last_name']. '  '.$donnees['first_name']; ?><br />
         L'émail de ce client est : <?php echo $donnees['email']; ?> et il habite à <?php echo $donnees['address'].'  '.$donnees['postcode'].' ' .$donnees['city']; ?>.<br />
-
-
     <?php
-}
-
-
+}echo"<br>";
 $r1->closeCursor(); // Termine le traitement de la requête
 }
 ?>
-
         <?php
+        echo"<br>";
         function oneCustomer (){
             $bdd=bddAccess();
         $r2= $bdd->query('SELECT first_name FROM customers WHERE  first_name=\'Charlize\'');
@@ -41,9 +38,81 @@ $r1->closeCursor(); // Termine le traitement de la requête
         ?>
     <strong>Le client demandé est </strong> : <?php echo $donnees['first_name']; ?><br />
     <?php
-    }
+    } echo"<br>";
     $r2->closeCursor();
         }
+
     ?>
+<?php
 
+function orderProducts (){
+    $bdd=bddAccess();
+    $r3= $bdd->query('SELECT products.name, order_product.quantity, products.price
+                               FROM products 
+                               INNER JOIN order_product ON(products.id=order_product.product_id)
+                               WHERE order_product.order_id = 1');
+    while ($donnees = $r3->fetch())
+    {
+        ?>
+        <strong>Les produits de la commande id=1 sont </strong> : <?php echo $donnees['name'];?><br/>
+        <strong> La quantité est de</strong> : <?php echo $donnees['quantity'];?><br/>
+        <strong> Le prix est de</strong> : <?php echo $donnees['price'];?><br/>
+        <?php
 
+    }   echo"<br>";
+    $r3->closeCursor();
+}
+?>
+
+<?php
+    function ordersCharlize (){
+    $bdd=bddAccess();
+    $r4= $bdd->query('SELECT orders.number, sum(order_product.quantity*products.price) AS totalPrice
+                               FROM orders 
+                               INNER JOIN order_product ON orders.id=order_product.order_id
+                               INNER JOIN products ON order_product.product_id=products.id
+                               INNER JOIN customers ON customers.id=orders.customer_id
+                               WHERE customers.first_name =\'Charlize\'
+                               Group by orders.number');
+    while ($donnees = $r4->fetch())
+    {
+    ?>
+    <strong>Liste des commandes</strong> : <?php echo $donnees['number'];?><br/>
+    <strong> Le prix total de la commande</strong> : <?php echo $donnees['totalPrice'];?><br/>
+
+<?php
+
+}echo"<br>";
+$r4->closeCursor();
+}
+?>
+
+<?php
+function insertOrder (){
+    $bdd=bddAccess();
+ $bdd->exec('INSERT INTO orders(date,number,customer_id) 
+                               VALUES (\'2021-06-08\',\'order00007\',2)');
+    echo "Commande ajoutée!";
+    echo"<br>";
+}
+?>
+<?php
+function deleteOrder ($id){
+    $bdd=bddAccess();
+    $req = $bdd->prepare('DELETE FROM `orders` WHERE `orders`.`id` = :id');
+    $req->execute(array(
+            'id'=>$id,
+    ));
+    echo "Commande effacée!";
+    }
+?>
+<?php
+function upDate ($id){
+    $bdd=bddAccess();
+    $req = $bdd->prepare('DELETE FROM `orders` WHERE `orders`.`id` = :id');
+    $req->execute(array(
+        'id'=>$id,
+    ));
+    echo "Commande effacée!";
+}
+?>
---
+++ tables.php
@@ -0,0 +1,9 @@
+<?php include "database.php";?>
+
+
+<?php echo allCustomers();?>
+<?php echo oneCustomer();?>
+<?php echo orderProducts();?>
+<?php echo ordersCharlize();?>
+<?php //echo insertOrder();?>
+<?php echo deleteOrder (7);?>
